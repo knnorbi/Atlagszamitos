dc7e635b538dbc686a74ca9e1f593ebc896ca666
knnorbi <kluka.norbert@gmail.com>
2020-10-01 11:48:57 +0200

    done.

--- Atlagszamitos/Program.cs
+++ Atlagszamitos/Program.cs
@@ -10,6 +10,32 @@ namespace Atlagszamitos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Add meg az évvégi eredményeidet!");
+            Console.Write("Magyar: ");
+            int magyar = int.Parse(Console.ReadLine());
+            Console.Write("Matek: ");
+            int matek = int.Parse(Console.ReadLine());
+            Console.Write("Töri: ");
+            int tori = int.Parse(Console.ReadLine());
+            Console.Write("Programozás: ");
+            int prog = int.Parse(Console.ReadLine());
+            Console.Write("Tesi: ");
+            int tesi = int.Parse(Console.ReadLine());
+
+            if (magyar < 1 || magyar > 5 ||
+                matek < 1 || matek > 5 ||
+                tori < 1 || tori > 5 ||
+                prog < 1 || prog > 5 ||
+                tesi < 1 || tori > 5)
+            {
+                Console.WriteLine("Hibás adatot adtál meg!");
+            }
+            else
+            {
+                double atlag = (magyar + matek + tori + prog + tesi) / 5.0;
+                Console.WriteLine("Az átlagod: " + atlag);
+            }
+            Console.ReadKey();
         }
     }
 }
